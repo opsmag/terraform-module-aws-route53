74f104fa982ba97e247a09a583505ba77a837a79
Boris Karaoglanov <bkaraoglanov@astraflare.com>
2020-03-05 01:21:15 -0800

    Initial Commit

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "zone_id" {
   value       = "${aws_route53_zone.this.zone_id}"
   description = "The hosted zone id"
 }
+
+output "zone_name" {
+  value       = "${aws_route53_zone.this.name}"
+  description = "The hosted zone name"
+}
